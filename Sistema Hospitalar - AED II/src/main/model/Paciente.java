54687e264c59c15c6c69f5e10d4b646071e5741d
Jeffrey_Jr. <jeffwilljr05@gmail.com>
2026-03-31 02:33:05 +0200

    i just add some comments no Paciente.java

--- Sistema Hospitalar - AED II/src/main/model/Paciente.java
+++ Sistema Hospitalar - AED II/src/main/model/Paciente.java
@@ -23,30 +23,30 @@ public class Paciente {
 
     //Getters 
     public int getId(){
-    return id;
+        return id;
     }
     public String getNome(){
-    return nome;
+        return nome;
     }    
 
     public int getIdade(){
-    return idade;
+        return idade;
     }
 
     public String getBi(){
-    return bi;
+        return bi;
     }
 
     public String getTelefone(){
-    return telefone;
+        return telefone;
     }
 
     public String getEndereco(){
-    return endereco;
+        return endereco;
     }
 
     public String getPrioridade(){
-    return prioridade;
+        return prioridade;
     }
 
 
@@ -85,3 +85,14 @@ public class Paciente {
     }
 
 }
+/*
+id pode ser gerado de forma automatica ou como parametro
+podemos txunar:
+- de forma automatica que seria criar um contador estatica que incrementa sempre:
+......private static int contadorId = 1;
+......public Paciente (String nome, int idade, String bi, String telefone, String endereco, String prioridade){
+..........this.id = contadorId++;
+
+- de forma manual que seria passar o id como parametro no construtor, isso pode ser interessante para facilitar a busca e remoção de pacientes, mas isso é algo que podemos discutir
+
+*/
