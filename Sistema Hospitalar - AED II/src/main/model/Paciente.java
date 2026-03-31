154c19f4a791c39cb46246a01b75cdbc9887a547
Jeffrey_Jr. <jeffwilljr05@gmail.com>
2026-03-31 02:15:57 +0200

    Criei construtor em Paciente.java para criar objectos da classe paciente getters e setters para aceder e modificar os atributos com facilidade e toString para facilitar a visualizacao do paciente

--- Sistema Hospitalar - AED II/src/main/model/Paciente.java
+++ Sistema Hospitalar - AED II/src/main/model/Paciente.java
@@ -8,4 +8,80 @@ public class Paciente {
     private String telefone;
     private String endereco;
     private String prioridade; // Pode ser "normal" ou "prioritario"
+
+   
+//Construtor para criar um paciente, o id pode ser gerado automaticamente ou passado como parametro, isso é algo que podemos discutir
+    public Paciente (int id, String nome, int idade, String bi, String telefone, String endereco, String prioridade){
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.bi = bi;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.prioridade = prioridade;
+    }
+
+    //Getters 
+    public int getId(){
+    return id;
+    }
+    public String getNome(){
+    return nome;
+    }    
+
+    public int getIdade(){
+    return idade;
+    }
+
+    public String getBi(){
+    return bi;
+    }
+
+    public String getTelefone(){
+    return telefone;
+    }
+
+    public String getEndereco(){
+    return endereco;
+    }
+
+    public String getPrioridade(){
+    return prioridade;
+    }
+
+
+    //Setters
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public void setBi(String bi){
+        this.bi = bi;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+    public void setPrioridade(String prioridade){
+        this.prioridade = prioridade;
+    }
+
+    public String toString(){
+        return "Paciente{" + 
+        "id=" + id + 
+        ", nome='" + nome + 
+        '\'' + ", idade=" + idade + 
+        ", bi='" + bi + 
+        '\'' + ", telefone='" + telefone + 
+        '\'' + ", endereco='" + endereco + 
+        '\'' + ", prioridade ='" + prioridade + '\'' + '}';
+    }
+
 }
