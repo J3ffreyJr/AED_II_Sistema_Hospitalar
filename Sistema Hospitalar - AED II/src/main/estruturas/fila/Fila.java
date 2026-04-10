b34551b0032b2bd6c5a6da6b3e43fdb21a0d4e49
Gueerco <guercoxavier1@gmail.com>
2026-04-10 22:06:01 +0200

    So Formatacao na classe fila

--- Sistema Hospitalar - AED II/src/main/estruturas/fila/Fila.java
+++ Sistema Hospitalar - AED II/src/main/estruturas/fila/Fila.java
@@ -1,85 +1,86 @@
 package estruturas.fila;
-public class Fila{
-private No primeiro, ultimo;
-private int tamanho;
+
+public class Fila {
+    private No primeiro, ultimo;
+    private int tamanho;
 
 
-public Fila() {
-    this.primeiro = null;
-    this.ultimo = null;
-    this.tamanho = 0;
-}
-
-public void enqueue(Object element) {
-    No novoNo = new No(element);
-
-    if (isEmpty()) {
-        ultimo = primeiro = novoNo;
-        // ultimo = novoNo;
-    } else {
-        ultimo.setProximo(novoNo);
-        ultimo = novoNo;
+    public Fila() {
+        this.primeiro = null;
+        this.ultimo = null;
+        this.tamanho = 0;
     }
-    tamanho++;
-}
 
-public void dequeue() {
-    if (isEmpty()) {
-        throw new IllegalStateException("Não é possível remover de uma fila vazia");
+    public void enqueue(Object element) {
+        No novoNo = new No(element);
+
+        if (isEmpty()) {
+            ultimo = primeiro = novoNo;
+            // ultimo = novoNo;
+        } else {
+            ultimo.setProximo(novoNo);
+            ultimo = novoNo;
+        }
+        tamanho++;
     }
-    primeiro = primeiro.getProximo();
-    tamanho--;
 
-    if (isEmpty()) {
-        ultimo = null;
+    public void dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Não é possível remover de uma fila vazia");
+        }
+        primeiro = primeiro.getProximo();
+        tamanho--;
+
+        if (isEmpty()) {
+            ultimo = null;
+        }
     }
-}
 
-public Object peek() {
-    if (isEmpty()) {
-        throw new IllegalStateException("Não é possível visualizar uma fila vazia");
+    public Object peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Não é possível visualizar uma fila vazia");
+        }
+        return primeiro.getElemento();
     }
-    return primeiro.getElemento();
-}
 
-public Object peekAndDequeue() {
-    Object elemento = peek();
-    dequeue();
-    return elemento;
-}
+    public Object peekAndDequeue() {
+        Object elemento = peek();
+        dequeue();
+        return elemento;
+    }
 
-public boolean isEmpty() {
+    public boolean isEmpty() {
 
-    return primeiro == null;
-}
+        return primeiro == null;
+    }
 
-public int size() {
-    return tamanho;
-}
+    public int size() {
+        return tamanho;
+    }
 
 
-public No getPrimeiro() {
-    return primeiro;
-}
+    public No getPrimeiro() {
+        return primeiro;
+    }
 
-public void setPrimeiro(No primeiro) {
-    this.primeiro = primeiro;
-}
+    public void setPrimeiro(No primeiro) {
+        this.primeiro = primeiro;
+    }
 
-public No getUltimo() {
-    return ultimo;
-}
+    public No getUltimo() {
+        return ultimo;
+    }
 
-public void setUltimo(No ultimo) {
-    this.ultimo = ultimo;
-}
+    public void setUltimo(No ultimo) {
+        this.ultimo = ultimo;
+    }
 
-public int getTamanho() {
-    return tamanho;
-}
+    public int getTamanho() {
+        return tamanho;
+    }
 
-public void setTamanho(int tamanho) {
-    this.tamanho = tamanho;
-}
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
 
 }
