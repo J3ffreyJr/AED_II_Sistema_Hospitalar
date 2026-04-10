c55f942261d86427af3166df2dfa7b936336c717
Gueerco <guercoxavier1@gmail.com>
2026-04-10 21:49:42 +0200

    Classe Fila implementada

--- Sistema Hospitalar - AED II/src/main/estruturas/fila/Fila.java
+++ Sistema Hospitalar - AED II/src/main/estruturas/fila/Fila.java
@@ -1,15 +1,85 @@
-public class Fila {
-    public void enqueue(Paciente paciente) {
-        // Lógica para adicionar um paciente à fila
-    }
+package estruturas.fila;
+public class Fila{
+private No primeiro, ultimo;
+private int tamanho;
 
-    public Paciente dequeue() {
-        // Lógica para remover e retornar o próximo paciente da fila
-        return null;
+
+public Fila() {
+    this.primeiro = null;
+    this.ultimo = null;
+    this.tamanho = 0;
+}
+
+public void enqueue(Object element) {
+    No novoNo = new No(element);
+
+    if (isEmpty()) {
+        ultimo = primeiro = novoNo;
+        // ultimo = novoNo;
+    } else {
+        ultimo.setProximo(novoNo);
+        ultimo = novoNo;
     }
-    public boolean isEmpty() {
-        // Lógica para verificar se a fila está vazia
-        return false;
+    tamanho++;
+}
+
+public void dequeue() {
+    if (isEmpty()) {
+        throw new IllegalStateException("Não é possível remover de uma fila vazia");
     }
-    
+    primeiro = primeiro.getProximo();
+    tamanho--;
+
+    if (isEmpty()) {
+        ultimo = null;
+    }
+}
+
+public Object peek() {
+    if (isEmpty()) {
+        throw new IllegalStateException("Não é possível visualizar uma fila vazia");
+    }
+    return primeiro.getElemento();
+}
+
+public Object peekAndDequeue() {
+    Object elemento = peek();
+    dequeue();
+    return elemento;
+}
+
+public boolean isEmpty() {
+
+    return primeiro == null;
+}
+
+public int size() {
+    return tamanho;
+}
+
+
+public No getPrimeiro() {
+    return primeiro;
+}
+
+public void setPrimeiro(No primeiro) {
+    this.primeiro = primeiro;
+}
+
+public No getUltimo() {
+    return ultimo;
+}
+
+public void setUltimo(No ultimo) {
+    this.ultimo = ultimo;
+}
+
+public int getTamanho() {
+    return tamanho;
+}
+
+public void setTamanho(int tamanho) {
+    this.tamanho = tamanho;
+}
+
 }
